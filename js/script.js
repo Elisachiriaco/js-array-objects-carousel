b62dc0e1ec977ca81f34a13e0a843cf8f6ed33d2
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-04-08 17:46:15 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-/*
-Consegna:
-Dati tre array contenenti:
- - una lista ordinata di 5 immagini,
- - una lista ordinata dei relativi 5 luoghi e
- - una lista di 5 news,
-creare un carosello come nella foto allegata.
-*/
-  const items = [
+ const items = [
       {
           img: 'img/01.jpg',
           titolo: 'Svezia',
@@ -35,14 +27,9 @@ creare un carosello come nella foto allegata.
   ]
   console.log(items);
 
-
-//variabile per raccogliere tutto l'html che va in items-container
+// variabili
 let itemTemplate = "";
-
-//variabile per raccogliere tutto l'html che va in thumbs-container
 let thumbTemplate = "";
-
-// preparo una varibile con l'indice dell'elemento attivo e la pongo inizialmente a 0 ovvero il primo elemento dell'array
 let currentIndexActive = 0;
 
 //eseguo il ciclo for sull'array delle immagini (items) e popolo l'html delle due varibaili da stampare nei due contenitori (immagini e thumbnails)
@@ -62,44 +49,33 @@ for (let i = 0; i < items.length; i++) {
   thumbTemplate += `
   <div class="thumb ${classActive}">
     <img src="${items[i].img}" alt="" />
-  </div>`;
-}
-//console.log(thumbTemplate);
+  </div>`
+};
 
 // metto in due variabili rispettivamente i contenitori che si trovano nell'html
 const itemsContainer = document.querySelector(".items-container");
 const thumbsContainer = document.querySelector(".thumbs-container");
-//console.log(itemContainer);
 
 //stampo l'html corrispondente nei due contenitori
 itemsContainer.innerHTML = itemTemplate;
 thumbsContainer.innerHTML += thumbTemplate;
-//document.querySelector(".item").classList.add("active");
 
 //Pulsanti
-//.next .fa-circle-chevron-down
-//.prev .fa-circle-chevron-up
-//metto nelle variabili next e prev i due pulsanti
 const next = document.querySelector(" .fa-circle-chevron-down");
 const prev = document.querySelector(" .fa-circle-chevron-up");
 const stop = document.querySelector(".fa-circle-pause");
 
-//console.log(next, prev);
 function slideDown() {
-  //prendere immagine con currentIndexActive e togliere classe active
   const imgs = document.getElementsByClassName("item");
   imgs[currentIndexActive].classList.remove("active");
   const thumbs = document.getElementsByClassName("thumb");
   thumbs[currentIndexActive].classList.remove("active");
-  //console.log(imgs);
   if (currentIndexActive === 4) {
     currentIndexActive = 0;
   } else {
     currentIndexActive++;
   }
-  //console.log(currentIndexActive);
   imgs[currentIndexActive].classList.add("active");
-  //console.log(currentIndexActive);
   thumbs[currentIndexActive].classList.add("active");
 }
 function slideUp() {
@@ -107,15 +83,14 @@ function slideUp() {
   imgs[currentIndexActive].classList.remove("active");
   const thumbs = document.getElementsByClassName("thumb");
   thumbs[currentIndexActive].classList.remove("active");
-  //console.log(imgs);
+
   if (currentIndexActive === 0) {
     currentIndexActive = items.length - 1;
   } else {
     currentIndexActive--;
   }
-  //console.log(currentIndexActive);
   imgs[currentIndexActive].classList.add("active");
-  //console.log(currentIndexActive);
+
   thumbs[currentIndexActive].classList.add("active");
 }
 
